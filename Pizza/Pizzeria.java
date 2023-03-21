e93ce79650898242767c25161ab779c316a67bd5
Miguel <62133840+Primtaxx@users.noreply.github.com>
2023-03-21 23:12:32 +0100

    +

--- Pizza/Pizzeria.java
+++ Pizza/Pizzeria.java
@@ -4,12 +4,12 @@ package Pizza;
 public abstract class Pizzeria {
    
     public abstract Pizza createPizza(String type);
+
     public void orderPizza(String type) {
         Pizza pizza = createPizza(type);
         pizza.bake();
         pizza.cut();
         pizza.box();
-        
     }
 
 }
